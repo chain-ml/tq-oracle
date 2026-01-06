02d86f4e0db72f7122cf91209f9611b398323062
enigmarikki <hrishi98.m@gmail.com>
2026-01-06 09:57:15 -0500

    "chore : use pyth only"

--- src/tq_oracle/adapters/price_adapters/__init__.py
+++ src/tq_oracle/adapters/price_adapters/__init__.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from .cow_swap import CowSwapAdapter
 from .eth import ETHAdapter
-
+from .pyth import PythAdapter
 PRICE_ADAPTERS = [
     CowSwapAdapter,
     ETHAdapter,
 ]
 
-__all__ = ["PRICE_ADAPTERS", "CowSwapAdapter", "ETHAdapter"]
+__all__ = ["PRICE_ADAPTERS", "PythAdapter", "ETHAdapter"]
